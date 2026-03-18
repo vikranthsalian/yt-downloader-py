3dbf16e2160c83f1251fb547aaabcde23f7371d7
Vikranth Salian <30863763+vikranthsalian@users.noreply.github.com>
2026-03-18 14:00:45 +0530

    Change default port from 5000 to 9999

--- main.py
+++ main.py
@@ -145,5 +145,5 @@ def available_resolutions():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 9999))
     app.run(host="0.0.0.0", port=port)
